ce04d204dced192e1440ace53248f3baff23137b
Prince Mathebula <nthavela202@gmail.com>
2026-08-05 02:08:36 +0200

    Fixed MongoDB connection string

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // ----- MONGODB CONNECTION -----
-const MONGO_URI = 'mongodb+srv://admin:nthavela202_db_user@cluster0.xxxxx.mongodb.net/?retryWrites=true&w=majority';
+const MONGO_URI = 'mongodb+srv://admin:nthavela202_db_user@cluster0.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose.connect(MONGO_URI, { dbName: 'chillbox' })
     .then(() => console.log('✅ Connected to MongoDB Atlas!'))
